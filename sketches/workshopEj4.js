2013c616f9067f6c41939c683484ebc7c42de42a
Jrozou26092000 <Jrozou26092000@users.noreply.github.com>
2022-04-04 02:20:05 +0000

    deploy: 1a129975059bfbc630a4c5e946473d40de7ed816

--- sketches/workshopEj4.js
+++ sketches/workshopEj4.js
@@ -1,29 +1,43 @@
 // took from here: https://github.com/VisualComputing/Cognitive/blob/gh-pages/sketches/scintillating_grid.js
 
 new p5((p) => {
-  let img;
+  let img, in1, in2, in3, in4, in5, in6, in7, in8, in9, button;
   //let input, button, greeting;
   //let w = 80;
   /* const matrix = [ [ 0, -1, 0 ],
-                 [ -1, 5, -1 ],
+                 [ -1, 4, -1 ],
                  [ 0, -1, 0 ] ];  */
-const matrix = [ [ 0, 0, 0 ],
-              [ 0, 1, 0 ],
-              [ 0, 0, 0 ] ]; 
+  const matrix = [ [ 0.0625, 0.125, 0.0625 ],
+              [ 0.125, 0.25, 0.125 ],
+              [ 0.0625, 0.125, 0.0625 ] ]; 
 
   p.preload = function(){
     img = p.loadImage("/showcase/sketches/flowers2.jpg");
   };
 
   p.setup = function () {
-    p.createCanvas(640, 640);
+    p.createCanvas(640, 900);
+
+    in1 = p.createInput();
+    in2 = p.createInput();
+    in3 = p.createInput();
+    in4 = p.createInput();
+    in5 = p.createInput();
+    in6 = p.createInput();
+    in7 = p.createInput();
+    in8 = p.createInput();
+    in9 = p.createInput();
+    button = p.createButton('Apply');
+    //button.position(0, 0);
+    button.mousePressed(kernel);
+
     img.loadPixels();
     p.pixelDensity(1);
     p.noLoop();
   };
 
   p.draw = function () {
-    p.background(img);
+    p.background(230);
     const matrixsize = 3;
 
     // Histogram
@@ -89,6 +103,20 @@ const matrix = [ [ 0, 0, 0 ],
     btotal = p.constrain(btotal, 0, 255);
     
     return p.color(rtotal, gtotal, btotal);
+  };
+
+  function kernel(){
+    matrix[0][0] = in1.value();
+    matrix[0][1] = in2.value();
+    matrix[0][2] = in3.value();
+    matrix[1][0] = in4.value();
+    matrix[1][1] = in5.value();
+    matrix[1][2] = in6.value();
+    matrix[2][0] = in7.value();
+    matrix[2][1] = in8.value();
+    matrix[2][2] = in9.value();
+
+    p.redraw();
   };
 }, "workshopEj4");
   
